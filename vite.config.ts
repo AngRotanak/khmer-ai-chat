ef1e72b0533f56bf07cffec1f84d0bd9a2616dec
Ang Rotanak <angrotanak@gmail.com>
2026-06-02 18:50:57 +0100

    Initial commit frontend

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import unocss from 'unocss/vite'
 import autoImport from 'unplugin-auto-import/vite'
-import unhead from '@unhead/addons/vite'
+import unhead from "@unhead/addons/vite";
+
+
 import path from 'path'
 
 export default defineConfig({
